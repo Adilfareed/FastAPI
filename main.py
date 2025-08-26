40b778240cf7c411ed629c254494a404e465af8d
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2025-08-26 20:37:56 +0500

    configured get method

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+import json
+
 
 app=FastAPI()
 
 @app.get("/")
 def hello():
     return("messange from backend :Hello")
+
+def load_data():
+    with open("patients.json","r") as f:
+        data=json.load(f)
+    return data 
+
+ 
+@app.get("/patients")
+def patients():
+    data=load_data()
+    return data   
+
+@app.get("/patient/{patient_id}")
+def patients(patient_id:str):
+    data=load_data()
+
+    if patient_id in data :
+        return data[patient_id] 
+    raise HTTPException(status_code=404, detail="Patient not found")
